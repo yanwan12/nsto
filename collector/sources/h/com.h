dd96d12d06ad583348c216be709b1e4a02da0d9d
LAPTOP-TJC0HOBS\R&D_Indus <ybe@newsteo.com>
2018-04-24 10:30:24 +0200

    Signed-off-by: LAPTOP-TJC0HOBS\R&D_Indus <ybe@newsteo.com>

--- collector/sources/h/com.h
+++
@@ -1,9 +0,0 @@
-// com.h
-// 21.11.2013
-
-void comInit  (void);
-int  comRead  (char * _pc_buffer, int _n_buffer_len);
-int  comWrite (char * _pc_buffer, int _n_buffer_len);
-
-// End of file
-
--- collector/sources/h/version.h
+++
@@ -1,6 +0,0 @@
-#define VAL_STR_VERSION		"1.0.0"
-
-
-/*
-1.0.0	20/11/2013	First version
-*/
